603f7a8101eefd607f0df5131f04fc4ab5774bcc
amit_ibriz <amit.khatri@ibriz.ai>
2022-12-02 19:59:27 +0545

    add primate v2 token test

---
+++ test/primatev2.test.js
@@ -0,0 +1,79 @@
+const {expect} = require('chai');
+const {ethers} = require('hardhat');
+
+describe('PRIMATEv2 Token contract', function () {
+  let primateV2Token;
+  let MINTER_ROLE = '0x6d696e7465720000000000000000000000000000000000000000000000000000';
+
+  beforeEach(async function () {
+    [owner, addr1, addr2] = await hre.ethers.getSigners();
+    const PrimateV2Token = await hre.ethers.getContractFactory('PRIMATEv2');
+    primateV2Token = await PrimateV2Token.deploy('PRIMATE v2', 'PRIMATE', '18', '0x539B86cD88fd41272335f9E46eAf7bF64f9Fa1e5');
+    await primateV2Token.deployed();
+  });
+
+  describe('Deploy Token', function () {
+    it('Should deploy successfully', async function () {
+      console.log('Success!!');
+    });
+    it('Should set the right owner', async function () {
+      expect(await primateV2Token.owner()).to.equal(owner.address);
+    });
+
+    it('Should set the right role, verify role and revoke role', async function () {
+      await primateV2Token.connect(owner).grantRole(MINTER_ROLE, owner.address);
+      expect(await primateV2Token.connect(owner).hasRole(MINTER_ROLE, owner.address)).to.equal(true);
+      await primateV2Token.connect(owner).revokeRole(MINTER_ROLE, owner.address);
+      expect(await primateV2Token.connect(owner).hasRole(MINTER_ROLE, owner.address)).to.equal(false);
+    });
+    it('Should verify the right owner after ownership transfer', async function () {
+      await primateV2Token.connect(owner).transferOwnership(addr1.address);
+      expect(await primateV2Token.owner()).to.equal(addr1.address);
+    });
+  });
+
+  describe('Mint Tokens & Transfer tokens', function () {
+    it("Should fail if sender doesn't have minter role", async function () {
+      await expect(primateV2Token.connect(owner).mint(addr1.address, 1)).to.be.revertedWith("AccessControl: missing 'minter' role");
+    });
+    it('Should successfully mint a token with minter role', async function () {
+      await primateV2Token.connect(owner).grantRole(MINTER_ROLE, owner.address);
+      await primateV2Token.connect(owner).mint(addr1.address, 1);
+      expect(await primateV2Token.balanceOf(addr1.address)).to.equals(1);
+    });
+    it('Should successfully batch mint tokens with minter role', async function () {
+      await primateV2Token.connect(owner).grantRole(MINTER_ROLE, owner.address);
+      await primateV2Token.connect(owner).batchMint([addr1.address, addr2.address], [1, 2]);
+      expect(await primateV2Token.balanceOf(addr1.address)).to.equals(1);
+      expect(await primateV2Token.balanceOf(addr2.address)).to.equals(2);
+    });
+    it('Should successfully transfer a token to other wallets', async function () {
+      await primateV2Token.connect(owner).grantRole(MINTER_ROLE, owner.address);
+      await primateV2Token.connect(owner).mint(addr1.address, 1);
+      expect(await primateV2Token.balanceOf(addr1.address)).to.equals(1);
+      await primateV2Token.connect(addr1).transfer(addr2.address, 1);
+      expect(await primateV2Token.balanceOf(addr1.address)).to.equals(0);
+      expect(await primateV2Token.balanceOf(addr2.address)).to.equals(1);
+    });
+    it('Should successfully batch transfer tokens to other wallets', async function () {
+      await primateV2Token.connect(owner).grantRole(MINTER_ROLE, owner.address);
+      await primateV2Token.connect(owner).mint(owner.address, 10);
+      expect(await primateV2Token.balanceOf(owner.address)).to.equals(10);
+      expect(await primateV2Token.balanceOf(addr1.address)).to.equals(0);
+      expect(await primateV2Token.balanceOf(addr1.address)).to.equals(0);
+      await primateV2Token.connect(owner).batchTransfer([addr1.address, addr2.address], [4, 5]);
+      expect(await primateV2Token.balanceOf(addr1.address)).to.equals(4);
+      expect(await primateV2Token.balanceOf(addr2.address)).to.equals(5);
+      expect(await primateV2Token.balanceOf(owner.address)).to.equals(1);
+    });
+  });
+  describe('Burn tokens', function () {
+    it('Should update balances after burn', async function () {
+      await primateV2Token.connect(owner).grantRole(MINTER_ROLE, owner.address);
+      await primateV2Token.connect(owner).mint(owner.address, 10);
+      expect(await primateV2Token.balanceOf(owner.address)).to.equals(10);
+      await primateV2Token.connect(owner).burn(10);
+      expect(await primateV2Token.balanceOf(owner.address)).to.equals(0);
+    });
+  });
+});
