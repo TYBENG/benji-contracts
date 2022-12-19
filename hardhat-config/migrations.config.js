e688b5105bb785c2e686990d30d7eee805d21841
amit_ibriz <amit.khatri@ibriz.ai>
2022-12-19 19:08:48 +0545

    refactor configs

--- hardhat-config/migrations.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  paths: {
-    deploy: 'migrations',
-  },
-};
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,11 @@
 const {mergeConfigs} = require('@animoca/ethereum-contracts/src/config');
-require('@animoca/ethereum-migrations/hardhat-plugins');
-require('./tasks');
+require('dotenv').config();
 
-module.exports = mergeConfigs(require('@animoca/ethereum-contracts/hardhat-config'), require('./hardhat-config'));
+require('@animoca/ethereum-contracts/hardhat-plugins');
+require('@animoca/ethereum-migrations/hardhat-plugins');
+
+module.exports = mergeConfigs(
+  require('@animoca/ethereum-contracts/hardhat-config'),
+  require('@animoca/ethereum-migrations/hardhat-config'),
+  require('./hardhat-config')
+);
