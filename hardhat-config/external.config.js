7720c581c4d9abdeea1f2a3a2ea558166e2a46b5
amit_ibriz <amit.khatri@ibriz.ai>
2022-12-19 19:10:36 +0545

    refactor configs

---
+++ hardhat-config/external.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  external: {
+    contracts: [
+      {
+        artifacts: [
+          'node_modules/@animoca/ethereum-contracts-0.3.1/artifacts',
+          'node_modules/@animoca/ethereum-contracts-bridging-0.1.2/artifacts',
+          'node_modules/@animoca/ethereum-migrations/imports',
+          'node_modules/@animoca/ethereum-migrations/deployments/all',
+        ],
+        deploy: 'node_modules/@animoca/ethereum-migrations/migrations',
+      },
+      {
+        artifacts: 'node_modules/@animoca/ethereum-contracts/artifacts',
+      },
+    ],
+    deployments: {
+      hardhat: ['node_modules/@animoca/ethereum-migrations/deployments/all'],
+      localhost: ['node_modules/@animoca/ethereum-migrations/deployments/all'],
+      mainnet: ['node_modules/@animoca/ethereum-migrations/deployments/mainnet', 'node_modules/@animoca/ethereum-migrations/deployments/all'],
+      goerli: ['node_modules/@animoca/ethereum-migrations/deployments/goerli', 'node_modules/@animoca/ethereum-migrations/deployments/all'],
+      matic: ['node_modules/@animoca/ethereum-migrations/deployments/matic', 'node_modules/@animoca/ethereum-migrations/deployments/all'],
+      mumbai: ['node_modules/@animoca/ethereum-migrations/deployments/mumbai', 'node_modules/@animoca/ethereum-migrations/deployments/all'],
+    },
+  },
+};
