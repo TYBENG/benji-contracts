7842adde62205eafe073abae9fc75c490d9ffd6e
amit_ibriz <amit.khatri@ibriz.ai>
2022-12-20 13:57:14 +0545

    add network configs

---
+++ hardhat-config/networks.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  networks: {
+    // Ethereum
+    mainnet: {
+      defaultProvider: 'alchemy',
+    },
+    goerli: {
+      defaultProvider: 'alchemy',
+      companionNetworks: null,
+    },
+  },
+};
