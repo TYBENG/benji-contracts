f2b4220397af43b2a5a0ef1cd5e7a5411bf714cf
amit_ibriz <amit.khatri@ibriz.ai>
2022-12-22 11:11:36 +0545

    refactor grant role script

--- migrations/002_set_MinterRole.js
+++ migrations/002_set_MinterRole.js
@@ -1,22 +1,12 @@
-module.exports = async (bre) => {
-  const {deployments, getNamedAccounts} = hre;
-  const {execute} = deployments;
-  const {Primate_Wallet} = await getNamedAccounts();
-  const {MINTER_ROLE} = require('../constants/constant');
-
-  console.log('++++++++++++++++++++++ Setting minter role to deployer  ++++++++++++++++++++++');
-  await execute(
-    'PRIMATEv2',
-    {
-      from: Primate_Wallet,
-      log: true,
-    },
-    'grantRole',
-    MINTER_ROLE,
-    Primate_Wallet
-  );
-};
-module.exports.skip = async () => {
-  return false; // skip after running it first time and set flag as required
-};
-module.exports.tags = ['Primate_v2_setMinter'];
+const grantRole = require('@animoca/ethereum-migrations/src/templates/access/AccessControl/grantRole');
+const name = 'PRIMATEv2';
+const ROLE = 'MINTER';
+const addressToGranRole = [
+  '0x443B1253e699d44D743DD231288Df4b3275Dec23',
+  '0xbBB4CA19c92AfD4c164c53f953f841790B928f60',
+  '0xD194D2eC8A86cD64921C621ED67fd808Fe362bF9',
+];
+module.exports = grantRole(name, ROLE, addressToGranRole, {
+  from: 'Primate_Wallet',
+});
+module.exports.tags = ['grantRole_PRIMATEv2'];
