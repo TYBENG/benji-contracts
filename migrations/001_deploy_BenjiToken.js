d36fef38a2bddb05755e463785ee4def9861e639
amit_ibriz <amit.khatri@ibriz.ai>
2022-12-23 16:07:56 +0545

    refactor migrations scripts

--- migrations/001_deploy_BenjiToken.js
+++ migrations/001_deploy_BenjiToken.js
@@ -1,36 +1,15 @@
-const {getForwarderRegistryAddress} = require('@animoca/ethereum-contracts/test/helpers/run');
-const {multiSkip, skipIfContractExists} = require("@animoca/ethereum-migrations/src/helpers/common");
+const Contract_deploy = require('@animoca/ethereum-migrations/src/templates/Contract/deploy');
+const {getContractAddress} = require('@animoca/ethereum-migrations/src/helpers/templates');
 
-module.exports = async ({getNamedAccounts, deployments, getChainId, getUnnamedAccounts}) => {
-  const {deploy} = deployments;
-  const {BenjiToken_Wallet} = await getNamedAccounts();
-
-  let networkChainId = await getChainId();
-  console.log(`getChainId: ${networkChainId}`);
-
-  let tokenInfo = {
-    tokenName: 'BENJI',
-    tokenSymbol: 'BENJI',
-    tokenDecimals: 18,
-  };
-
-  const forwarderRegistryAddress = await getForwarderRegistryAddress();
-
-  await deploy('BenjiToken', {
-    from: BenjiToken_Wallet,
-    args: [tokenInfo.tokenName, tokenInfo.tokenSymbol, tokenInfo.tokenDecimals, forwarderRegistryAddress],
-    log: true,
-    autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
-  });
-};
-
-module.exports.skip = multiSkip([
-  // skipIfChainIdIs(['1']), // migration already done
-  // skipIfChainIdIs(['80001']), // migration already done
-  // skipIfChainIdIs(['5']), // migration already done for goerli
-  // skipIfChainIdIs(['137']), // migration already done
-  // skipIfChainIdIs(['97']), // bsctest
-  // skipIfChainIdIs(['56']), // bsc mainnet
-  skipIfContractExists('BenjiToken'), // contract guard
-]);
-module.exports.tags = ['BenjiToken'];
+module.exports = Contract_deploy('BenjiToken', {
+  contract: 'BenjiToken',
+  args: [
+    {name: 'tokenName', value: 'BENJI'},
+    {name: 'tokenSymbol', value: 'BENJI'},
+    {name: 'tokenDecimals', value: '18'},
+    {name: 'forwarderRegistry', value: getContractAddress('ForwarderRegistry@0.3.1')},
+  ],
+  deterministicDeployment: false,
+  from: 'BenjiToken_Wallet',
+});
+module.exports.tags = ['BenjiToken_deploy'];
