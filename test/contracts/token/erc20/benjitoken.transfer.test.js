e0e4e9e0f8c1ba62992b702348b85e7f3ee30688
sagarduwal <sagar@ibriz.ai>
2023-01-04 14:31:06 +0545

    :art: [change] optimizations for tests

--- test/contracts/token/erc20/benjitoken.transfer.test.js
+++ test/contracts/token/erc20/benjitoken.transfer.test.js
@@ -16,22 +16,22 @@ describe('BenjiTokenTransfer', function () {
   const fixture = async function () {
     const forwarderRegistryAddress = await getForwarderRegistryAddress();
     console.log(forwarderRegistryAddress);
-    this.primateToken = await deployContract('BenjiTokenMock', name, symbol, decimals, forwarderRegistryAddress);
-    this.sale = await deployContract('SaleContract', this.primateToken.address);
+    this.benjiToken = await deployContract('BenjiTokenMock', name, symbol, decimals, forwarderRegistryAddress);
+    this.sale = await deployContract('SaleContract', this.benjiToken.address);
 
-    await this.primateToken.grantRole(await this.primateToken.MINTER_ROLE(), deployer.address);
+    await this.benjiToken.grantRole(await this.benjiToken.MINTER_ROLE(), deployer.address);
   };
   beforeEach(async function () {
     await loadFixture(fixture, this);
   });
   describe('onTokenTransfer', function () {
     it('Should be able to transfer approvals', async function () {
-      await this.primateToken.connect(deployer).mint(deployer.address, 10);
-      expect(await this.primateToken.balanceOf(deployer.address)).to.equals(10);
-      await this.primateToken.connect(deployer).approve(this.sale.address, 5);
+      await this.benjiToken.connect(deployer).mint(deployer.address, 10);
+      expect(await this.benjiToken.balanceOf(deployer.address)).to.equals(10);
+      await this.benjiToken.connect(deployer).approve(this.sale.address, 5);
       await this.sale.connect(deployer).transferToken(deployer.address, addr1.address, 2);
-      expect(await this.primateToken.balanceOf(addr1.address)).to.equals(2);
-      expect(await this.primateToken.balanceOf(deployer.address)).to.equals(8);
+      expect(await this.benjiToken.balanceOf(addr1.address)).to.equals(2);
+      expect(await this.benjiToken.balanceOf(deployer.address)).to.equals(8);
     });
   });
 });
