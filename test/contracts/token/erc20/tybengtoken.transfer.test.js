b1a4f907d951a9e938447897ce29bf0e89bcff06
sagarduwal <sagarduwal0102@outlook.com>
2024-03-28 22:25:39 +0545

    :art: [change] token name changes to TYBENG

---
+++ test/contracts/token/erc20/tybengtoken.transfer.test.js
@@ -0,0 +1,48 @@
+const {expect} = require('chai');
+const {getForwarderRegistryAddress} = require('@animoca/ethereum-contracts/test/helpers/registries');
+const {loadFixture} = require('@animoca/ethereum-contract-helpers/src/test/fixtures');
+const {deployContract} = require('@animoca/ethereum-contract-helpers/src/test/deploy');
+const unit = require('ethjs-unit');
+
+describe('TYBENGTokenTransfer', function () {
+  let deployer, addr1, addr2;
+  const name = 'TYBENG v2 MOCK';
+  const symbol = 'MOCK TYBENG';
+  const decimals = ethers.BigNumber.from('18');
+
+  before(async function () {
+    [deployer, addr1, addr2] = await hre.ethers.getSigners();
+  });
+
+  const fixture = async function () {
+    const forwarderRegistryAddress = await getForwarderRegistryAddress();
+    let tokenBalance = unit.toWei('10', 'ether').toString();
+
+    const BatchMintAmounts = [tokenBalance];
+
+    const InitialHolderWallets = [deployer.address];
+
+    this.tybengToken = await deployContract(
+      'TYBENGTokenMock',
+      name,
+      symbol,
+      decimals,
+      InitialHolderWallets,
+      BatchMintAmounts,
+      forwarderRegistryAddress
+    );
+    this.sale = await deployContract('SaleContract', this.tybengToken.address);
+  };
+  beforeEach(async function () {
+    await loadFixture(fixture, this);
+  });
+  describe('onTokenTransfer', function () {
+    it('Should be able to transfer approvals', async function () {
+      expect(await this.tybengToken.balanceOf(deployer.address)).to.equals(unit.toWei('10', 'ether').toString());
+      await this.tybengToken.connect(deployer).approve(this.sale.address, unit.toWei('5', 'ether').toString());
+      await this.sale.connect(deployer).transferToken(deployer.address, addr1.address, unit.toWei('2', 'ether').toString());
+      expect(await this.tybengToken.balanceOf(addr1.address)).to.equals(unit.toWei('2', 'ether').toString());
+      expect(await this.tybengToken.balanceOf(deployer.address)).to.equals(unit.toWei('8', 'ether').toString());
+    });
+  });
+});
